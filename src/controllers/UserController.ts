0add81b56fc765a33937a2ed0890ffbefd0c6cb1
Cristian Silva <cristian123105@gmail.com>
2020-08-19 18:44:32 -0300

    chore: add task and user migrante and create user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
+import User from '../models/UserModel';
 
 class UserController {
   store(req: Request, res: Response) {
+    User.create({ name: 'cristian', email: 'cristianemail@email.com', password: '1231231' });
     return res.status(200).send({ message: 'ok' });
   }
 }
