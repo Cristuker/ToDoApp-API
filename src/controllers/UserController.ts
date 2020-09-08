93e165286c5e3fa18f35fdb2486be6df0a4bf79a
Cristian Silva <cristian123105@gmail.com>
2020-09-08 19:26:28 -0300

    feat: ensure return 401 when missing params

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,8 +1,19 @@
+import * as Yup from 'yup';
 import { Request, Response } from 'express';
 
 class UserController {
-  store(req: Request, res: Response) {
-    return res.status(200).send({ message: 'ok' });
+  async store(request: Request, response: Response) {
+    const schema = Yup.object({
+      name: Yup.string().required().min(2),
+      email: Yup.string().required().email(),
+      password: Yup.string().required().min(4),
+      passwordConfirmation: Yup.string().required().min(4),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(401).send('Invalid request, please check the body');
+    }
+    return response.status(200).json('Created');
   }
 }
 
